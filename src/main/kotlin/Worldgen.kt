af1fc04d18bb57bf9f4f04ceac5d5e796974d7d5
Stefan Oltmann <github@stefan-oltmann.de>
2026-04-25 23:14:46 +0200

    AI slop

--- src/main/kotlin/Worldgen.kt
+++ src/main/kotlin/Worldgen.kt
@@ -117,7 +117,33 @@ object Worldgen {
     }
 
     fun generate(coordinate: String): String {
-        val result = nodeRuntime.getExecutor("JSON.stringify(worldgen.generate('$coordinate'))").executeString()
+        val result = nodeRuntime.getExecutor(
+            "const r = JSON.parse(worldgen.generate('$coordinate'));" +
+            "delete r.element_table;" +
+            "for (const world of r.worlds) {" +
+            "    delete world.element_idx;" +
+            "    delete world.mass;" +
+            "    delete world.temperature;" +
+            "    delete world.disease_idx;" +
+            "    delete world.disease_count;" +
+            "    delete world.pickupables;" +
+            "    for (const cell of world.biome_cells) { delete cell.type; }" +
+            "    for (const geyserSpawn of world.geysers) { delete geyserSpawn.cell; }" +
+            "    for (const entitySpawn of world.other_entities) { delete entitySpawn.cell; }" +
+            "    for (const entitySpawn of world.buildings) {" +
+            "        delete entitySpawn.cell;" +
+            "        delete entitySpawn.connections;" +
+            "        delete entitySpawn.rotationOrientation;" +
+            "    }" +
+            "}" +
+            "for (const poi of r.starmap_pois) {" +
+            "    delete poi.capacity_roll;" +
+            "    delete poi.recharge_roll;" +
+            "    delete poi.total_capacity;" +
+            "    delete poi.recharge_time;" +
+            "}" +
+            "JSON.stringify(r);"
+        ).executeString()
         return result
     }
 }
