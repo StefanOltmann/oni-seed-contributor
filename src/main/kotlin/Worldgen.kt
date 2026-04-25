a453a172b7ad34606769d6f6abd75dd3ec1caf9c
Stefan Oltmann <github@stefan-oltmann.de>
2026-04-25 11:30:26 +0200

    Added TODO

---
+++ src/main/kotlin/Worldgen.kt
@@ -0,0 +1,27 @@
+/*
+ * ONI Contribitor service
+ * Copyright (C) 2026 Stefan Oltmann
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+object Worldgen {
+
+    fun generate(coordinate: String): String {
+
+        // TODO Call the WASM worldgen service here.
+
+        return ""
+    }
+}
